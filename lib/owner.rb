b9da674da6477b16a1ff31b6d3cca3f0191f1eb4
Matteo Piccini <gnappo.1@live.it>
2019-11-05 00:04:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    save
+  end
+  
+  def say_species
+    species
+  end
+  
+  def self.all
+    @@all.dup.freeze
+  end
+  
+  def self.count
+    all.count
+  end
+  
+  def self.reset_all
+    all.reset
+  end
+  
+  private
+  def save
+    @@all << self
+  end
 end
